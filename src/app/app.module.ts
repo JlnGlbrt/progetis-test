4219fb4fd6daf49a2af7ca313230170d2f43eaa7
Julien Guilbert <j.guilbert@giesima.fr>
2022-08-09 22:46:14 +0200

    Création des modules Overview et Asteroids. Paramétrage du routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,39 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { OverviewComponent } from './overview/overview.component';
+import { AsteroidsComponent } from './asteroids/asteroids.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent,
+    AsteroidsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    RouterModule.forRoot([
+        {path: 'overview', component: OverviewComponent},
+        {path: 'asteroids', component: AsteroidsComponent},
+        {path: '', redirectTo: '/overview', pathMatch: 'full'},
+        {path: '**', component: PageNotFoundComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
